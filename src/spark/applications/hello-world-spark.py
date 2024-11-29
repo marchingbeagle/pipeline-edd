2e6ab7fbfa0863d8310d56b768f804fee638953b
Erik Schneider <erik.schneider.pacheco@gmail.com>
2024-11-28 21:29:07 -0300

    arquivos adicionados

---
+++ src/spark/applications/hello-world-spark.py
@@ -0,0 +1,12 @@
+import sys
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession.builder.appName("Hello World").getOrCreate()
+
+csv_file = sys.argv[1]
+
+df = spark.read.csv(csv_file, header=True, inferSchema= True)
+
+df.printSchema()
